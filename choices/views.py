e094a83032f68d1d39f59761a137a972658cbd76
Klodd <c.perdigou@gmail.com>
2026-03-01 23:14:37 +0100

    no registration ATM

--- choices/views.py
+++ choices/views.py
@@ -366,12 +366,12 @@ def admin_dashboard(request):
     }
     return render(request, 'admin/dashboard.html', context)
 
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('choices:index')
+# def register(request):
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('choices:index')
 
 
